a8e9757854a9a37e227e4d5ebae8ec1c6afcf12a
Kaung Myat Shwe <225817956+kaungmyatshwe1397@users.noreply.github.com>
2026-05-23 14:42:49 +0700

    feat:add purchase route and ticke purchase logic

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
 import express, { urlencoded } from "express";
 import concertRoute from "./routes/concert.route";
-import reservationRoute from './routes/reservation.route';
-import 'reflect-metadata';
+import reservationRoute from "./routes/reservation.route";
+import purchaseRoute from "./routes/purchase.route";
+import "reflect-metadata";
 import { AppDataSource } from "./database/data-source";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send('Hello,World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello,World");
+});
 
-app.use('/concerts',concertRoute);
+app.use("/concerts", concertRoute);
+app.use("/reserves", reservationRoute);
+app.use("/purchases",purchaseRoute);
 
-app.use('/reserves',reservationRoute);
+
+
 
 AppDataSource.initialize()
   .then(() => {
-    console.log('Database connected')
+    console.log("Database connected");
     app.listen(port, () => {
-      console.log(`Server running on port ${port}`)
-    })
+      console.log(`Server running on port ${port}`);
+    });
   })
   .catch((err) => {
-    console.error('Database connection failed', err)
-  })
+    console.error("Database connection failed", err);
+  });
---
+++ src/routes/purchase.route.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { AppDataSource } from "../database/data-source";
+import { Ticket } from "../entities/ticket";
+
+const router = Router();
+// Purchasing ticket
+router.post("/", async(req , res)=>{
+    const ticketId = req.body.ticketId;
+    // Check ticket is reseverd or not by pending status , and it match id from request body or not
+    const purchasedTicket = await AppDataSource.getRepository(Ticket).findOne({
+        where:{
+            id:ticketId,
+            status:"PENDING"
+        }
+    })
+    // Throwing error message if ticket is not valid or update status if purchase is success
+    if(!purchasedTicket){
+        return res.status(400).json({message:"Valid ticket is not found."})
+    }
+    purchasedTicket.status = "COMPLETED";
+    await AppDataSource.getRepository(Ticket).save(purchasedTicket);
+    return res.status(200).json({message:"Your ticket purchasing is success."})
+});
+
+
+export default router;
