e96112643e1ba11c9d46795c10c002d8879fa21d
Kaung Myat Shwe <225817956+kaungmyatshwe1397@users.noreply.github.com>
2026-05-25 12:48:51 +0700

    feat: add logs for getting concerts and ticket reservation

--- src/routes/reservation.route.ts
+++ src/routes/reservation.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AppDataSource } from "../database/data-source";
 import { Ticket } from "../entities/ticket";
 import { Concert } from "../entities/concert";
+import { getLogger } from "../logger";
 
 const router = Router();
 
@@ -16,11 +17,11 @@ router.post("/", async (req, res) => {
 
   // Write all logic event that want in bundle write inside of this
   try {
+    getLogger().info("Ticket reservation request received.");
     // Find and select the wanted concert by using concert id
-    const selectedConcert =
-      await queryRunner.manager.findOne(Concert, {
-        where: { id: concertId },
-      });
+    const selectedConcert = await queryRunner.manager.findOne(Concert, {
+      where: { id: concertId },
+    });
     // If there is no concert or concert have no ticket, return this
     if (!selectedConcert || selectedConcert.stock == 0) {
       await queryRunner.rollbackTransaction();
@@ -30,10 +31,7 @@ router.post("/", async (req, res) => {
     selectedConcert.stock -= 1;
 
     // Update the concert state
-    await queryRunner.manager.save(
-      Concert,
-      selectedConcert,
-    );
+    await queryRunner.manager.save(Concert, selectedConcert);
     // Create a new ticket for user for selectecd concert by using ticket entities
     await queryRunner.manager.save(Ticket, {
       userId: userId,
@@ -45,14 +43,13 @@ router.post("/", async (req, res) => {
     // The whole bundle process is success and do transaction commiting
     await queryRunner.commitTransaction();
     res.json({ message: "ticket is reserved." });
-  } 
-  // Roll back error happen in one of the things (ALL or Nothing)
-  catch (error) {
+  } catch (error) {
+    // Roll back error happen in one of the things (ALL or Nothing)
     await queryRunner.rollbackTransaction();
+    getLogger().warn("Reservation failed!!!");
     res.status(500).json({ message: "Ticket reservation failed!!!" });
-  } 
-  // Leave from lock transaction
-  finally {
+  } finally {
+    // Leave from lock transaction
     await queryRunner.release();
   }
 });
