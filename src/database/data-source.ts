ddd7665682699bc8da488bda4bc4cd8055c1926e
Kaung Myat Shwe <225817956+kaungmyatshwe1397@users.noreply.github.com>
2026-05-26 12:22:23 +0700

    feat:add user entities and run migration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
 import { Concert } from "../entities/concert";
 import { Ticket } from "../entities/ticket";
+import { User } from "../entities/user";
 
 
 export const AppDataSource = new DataSource({
     type: 'better-sqlite3',
   database: 'database.sqlite',
-  entities: [Concert,Ticket],
+  entities: [Concert,Ticket,User],
   migrations: [__dirname + "/../migrations/**/*{.js,.ts}"],
   synchronize: false,
 })
